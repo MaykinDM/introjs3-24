99494addd6088d08aade25c4020d2117fae5a2a6
Maycon Douglas <126088437+MaykinDM@users.noreply.github.com>
2024-03-06 21:10:55 -0300

    Add files via upload

---
+++ INTRODUCAOJS-3-24/1_tipos_primitivos/1_number/script.js
@@ -0,0 +1,17 @@
+//tipo number
+
+const meuNuero = 24;
+
+const telefone = 44991417642;
+
+const idade = 29;
+
+
+console.log(meuNuero);
+console.log(telefone);
+console.log(idade);
+
+
+const soma = meuNuero + idade;
+
+console.log(soma)
